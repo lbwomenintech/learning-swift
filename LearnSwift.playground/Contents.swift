0967b85803d1e5111f143212c118de383b8d029c
Owen Pierce <opierce@gmail.com>
2020-06-17 20:19:58 -0700

    Adding the LearnSwift playground with work from Variables, Constants, and Datatypes

---
+++ LearnSwift.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting: String = "Hello, playground"
+
+print(greeting.count)
+
+print(greeting.isEmpty)
+
+greeting.append("!")
+
+print(greeting)
+
+var isAString: Bool = true
+
+var myAge: Int = 20
+print(myAge + 10)
+print(myAge - 10)
+print(myAge * 10)
+print(myAge / 8)
+print(myAge % 8)
+
+var myBalance: Float = 50.0
+print(myBalance + 10)
+print(myBalance - 10)
+print(myBalance * 10)
+print(myBalance / 20)
+print(myBalance.truncatingRemainder(dividingBy: 20))
